b248f77464aed713659d983359ccf961c2c3e592
Rohit <102695143+rohit8168@users.noreply.github.com>
2024-01-07 19:56:15 +0530

    Update laptop.py

--- laptop.py
+++ laptop.py
@@ -5,7 +5,7 @@
 
 
 import pandas as pd 
-import matplotlib.pyplot as plt
+
 import numpy as np
 
 
@@ -62,8 +62,7 @@ X=df.copy()
 
 
 x=df.value_counts(df['brand'])
-df['brand'].value_counts().plot(kind="pie",autopct="%2.2f%%")
-plt.show()
+
 
 
 # In[9]:
